bd71b43c34ab5cd3e4e94ae8efe69c1d657ba092
Michael Yanni <miyanni@microsoft.com>
2016-09-02 11:36:51 -0700

    Removed the quantization from the palette. Ew... the colors are very moot and bland. This is because of averaging and those strong colors get blended away. I want to output a visual to inspect, so I'm gonna do that next.

--- ImageTracerNet/ImageTracer.cs
+++ ImageTracerNet/ImageTracer.cs
@@ -36,12 +36,12 @@ namespace ImageTracerNet
         }
 
         // Loading an image from a file, tracing when loaded, then returning IndexedImage with tracedata in layers
-        public static IndexedImage ImageToTraceData(string filename, Options options, byte[][] palette) 
+        internal static IndexedImage ImageToTraceData(string filename, Options options, byte[][] palette) 
         {
             return ImageToTraceData(new Bitmap(filename), options, palette);
         }
 
-        public static IndexedImage ImageToTraceData(Bitmap image, Options options, byte[][] palette) 
+        internal static IndexedImage ImageToTraceData(Bitmap image, Options options, byte[][] palette) 
         {
             return ImageDataToTraceData(image, LoadImageData(image), options, palette);
         }
@@ -78,8 +78,7 @@ namespace ImageTracerNet
             }
 
             // 1. Color quantization
-            //TODO: Remove Quantization logic while using SmartPalette.
-            var ii = ColorQuantization(imgd, colorPalette, options);
+            var ii = IndexedImage.Create(imgd, colorPalette, options.ColorQuantization);
             // 2. Layer separation and edge detection
             var rawlayers = Layering(ii);
             // 3. Batch pathscan
@@ -105,76 +104,76 @@ namespace ImageTracerNet
         // -1  0  0  0  0 -1
         // -1  0  0  0  0 -1
         // -1 -1 -1 -1 -1 -1
-        private static int[][] CreateIndexedColorArray(int height, int width)
-        {
-            height += 2;
-            width += 2;
-            return new int[height][].Initialize(i =>
-            i == 0 || i == height - 1
-                ? new int[width].Initialize(-1)
-                : new int[width].Initialize(-1, 0, width - 1));
-        }
+        //private static int[][] CreateIndexedColorArray(int height, int width)
+        //{
+        //    height += 2;
+        //    width += 2;
+        //    return new int[height][].Initialize(i =>
+        //    i == 0 || i == height - 1
+        //        ? new int[width].Initialize(-1)
+        //        : new int[width].Initialize(-1, 0, width - 1));
+        //}
 
         // 1. Color quantization repeated "cycles" times, based on K-means clustering
         // https://en.wikipedia.org/wiki/Color_quantization
         // https://en.wikipedia.org/wiki/K-means_clustering
-        private static IndexedImage ColorQuantization(ImageData imageData, Color[] colorPalette, Options options)
-        {
-            var arr = CreateIndexedColorArray(imageData.Height, imageData.Width);
-            // Repeat clustering step "cycles" times
-            for (var cycleCount = 0; cycleCount < options.ColorQuantization.ColorQuantCycles; cycleCount++)
-            {
-                // Reseting palette accumulator for averaging
-                var accumulatorPaletteIndexer = Enumerable.Range(0, colorPalette.Length).ToDictionary(i => i, i => new PaletteAccumulator());
+        //private static IndexedImage ColorQuantization(ImageData imageData, Color[] colorPalette, Options options)
+        //{
+        //    var arr = CreateIndexedColorArray(imageData.Height, imageData.Width);
+        //    // Repeat clustering step "cycles" times
+        //    for (var cycleCount = 0; cycleCount < options.ColorQuantization.ColorQuantCycles; cycleCount++)
+        //    {
+        //        // Reseting palette accumulator for averaging
+        //        var accumulatorPaletteIndexer = Enumerable.Range(0, colorPalette.Length).ToDictionary(i => i, i => new PaletteAccumulator());
 
-                for (var j = 0; j < imageData.Height; j++)
-                {
-                    for (var i = 0; i < imageData.Width; i++)
-                    {
-                        var pixel = imageData.Colors[j * imageData.Width + i];
-                        var distance = 256 * 4;
-                        var paletteIndex = 0;
-                        // find closest color from palette by measuring (rectilinear) color distance between this pixel and all palette colors
-                        for (var k = 0; k < colorPalette.Length; k++)
-                        {
-                            var color = colorPalette[k];
-                            // In my experience, https://en.wikipedia.org/wiki/Rectilinear_distance works better than https://en.wikipedia.org/wiki/Euclidean_distance
-                            var newDistance = color.CalculateRectilinearDistance(pixel);
+        //        for (var j = 0; j < imageData.Height; j++)
+        //        {
+        //            for (var i = 0; i < imageData.Width; i++)
+        //            {
+        //                var pixel = imageData.Colors[j * imageData.Width + i];
+        //                var distance = 256 * 4;
+        //                var paletteIndex = 0;
+        //                // find closest color from palette by measuring (rectilinear) color distance between this pixel and all palette colors
+        //                for (var k = 0; k < colorPalette.Length; k++)
+        //                {
+        //                    var color = colorPalette[k];
+        //                    // In my experience, https://en.wikipedia.org/wiki/Rectilinear_distance works better than https://en.wikipedia.org/wiki/Euclidean_distance
+        //                    var newDistance = color.CalculateRectilinearDistance(pixel);
 
-                            if (newDistance >= distance) continue;
+        //                    if (newDistance >= distance) continue;
 
-                            distance = newDistance;
-                            paletteIndex = k;
-                        }
+        //                    distance = newDistance;
+        //                    paletteIndex = k;
+        //                }
 
-                        // add to palettacc
-                        accumulatorPaletteIndexer[paletteIndex].Accumulate(pixel);
-                        arr[j + 1][i + 1] = paletteIndex;
-                    }
-                }
+        //                // add to palettacc
+        //                accumulatorPaletteIndexer[paletteIndex].Accumulate(pixel);
+        //                arr[j + 1][i + 1] = paletteIndex;
+        //            }
+        //        }
 
-                // averaging paletteacc for palette
-                for (var k = 0; k < colorPalette.Length; k++)
-                {
-                    // averaging
-                    if (accumulatorPaletteIndexer[k].A > 0) // Non-transparent accumulation
-                    {
-                        colorPalette[k] = accumulatorPaletteIndexer[k].CalculateAverage();
-                    }
+        //        // averaging paletteacc for palette
+        //        for (var k = 0; k < colorPalette.Length; k++)
+        //        {
+        //            // averaging
+        //            if (accumulatorPaletteIndexer[k].A > 0) // Non-transparent accumulation
+        //            {
+        //                colorPalette[k] = accumulatorPaletteIndexer[k].CalculateAverage();
+        //            }
 
-                    //https://github.com/jankovicsandras/imagetracerjava/issues/2
-                    // Randomizing a color, if there are too few pixels and there will be a new cycle
-                    if (cycleCount >= options.ColorQuantization.ColorQuantCycles - 1) continue;
-                    var ratio = accumulatorPaletteIndexer[k].Count / (double)(imageData.Width * imageData.Height);
-                    if ((ratio < options.ColorQuantization.MinColorRatio) && (cycleCount < options.ColorQuantization.ColorQuantCycles - 1))
-                    {
-                        colorPalette[k] = ColorExtensions.RandomColor();
-                    }
-                }
-            }
+        //            //https://github.com/jankovicsandras/imagetracerjava/issues/2
+        //            // Randomizing a color, if there are too few pixels and there will be a new cycle
+        //            if (cycleCount >= options.ColorQuantization.ColorQuantCycles - 1) continue;
+        //            var ratio = accumulatorPaletteIndexer[k].Count / (double)(imageData.Width * imageData.Height);
+        //            if ((ratio < options.ColorQuantization.MinColorRatio) && (cycleCount < options.ColorQuantization.ColorQuantCycles - 1))
+        //            {
+        //                colorPalette[k] = ColorExtensions.RandomColor();
+        //            }
+        //        }
+        //    }
 
-            return new IndexedImage(arr, colorPalette.Select(c => c.ToRgbaByteArray()).ToArray());
-        }
+        //    return new IndexedImage(arr, colorPalette.Select(c => c.ToRgbaByteArray()).ToArray());
+        //}
 
         // 2. Layer separation and edge detection
         // Edge node types ( ▓:light or 1; ░:dark or 0 )
--- ImageTracerNet/IndexedImage.cs
+++ ImageTracerNet/IndexedImage.cs
@@ -1,9 +1,14 @@
-﻿using TriListDoubleArray = System.Collections.Generic.List<System.Collections.Generic.List<System.Collections.Generic.List<double[]>>>;
+﻿using System.Drawing;
+using System.Linq;
+using ImageTracerNet.Extensions;
+using ImageTracerNet.OptionTypes;
+using ImageTracerNet.Palettes;
+using TriListDoubleArray = System.Collections.Generic.List<System.Collections.Generic.List<System.Collections.Generic.List<double[]>>>;
 
 namespace ImageTracerNet
 {
     // Container for the color-indexed image before and tracedata after vectorizing
-    public class IndexedImage
+    internal class IndexedImage
     {
         public int Width { get; }
         public int Height { get; }
@@ -21,6 +26,58 @@ namespace ImageTracerNet
             // Color quantization adds +2 to the original width and height
             Width = array[0].Length - 2;
             Height = array.Length - 2;
+        }
+
+        // Creating indexed color array arr which has a boundary filled with -1 in every direction
+        // Example: 4x4 image becomes a 6x6 matrix:
+        // -1 -1 -1 -1 -1 -1
+        // -1  0  0  0  0 -1
+        // -1  0  0  0  0 -1
+        // -1  0  0  0  0 -1
+        // -1  0  0  0  0 -1
+        // -1 -1 -1 -1 -1 -1
+        private static int[][] CreateIndexedColorArray(int height, int width)
+        {
+            height += 2;
+            width += 2;
+            return new int[height][].Initialize(i =>
+            i == 0 || i == height - 1
+                ? new int[width].Initialize(-1)
+                : new int[width].Initialize(-1, 0, width - 1));
+        }
+
+        public static IndexedImage Create(ImageData imageData, Color[] colorPalette, ColorQuantization colorQuant)
+        {
+            var arr = CreateIndexedColorArray(imageData.Height, imageData.Width);
+            // Repeat clustering step "cycles" times
+            for (var cycleCount = 0; cycleCount < colorQuant.ColorQuantCycles; cycleCount++)
+            {
+                for (var j = 0; j < imageData.Height; j++)
+                {
+                    for (var i = 0; i < imageData.Width; i++)
+                    {
+                        var pixel = imageData.Colors[j * imageData.Width + i];
+                        var distance = 256 * 4;
+                        var paletteIndex = 0;
+                        // find closest color from palette by measuring (rectilinear) color distance between this pixel and all palette colors
+                        for (var k = 0; k < colorPalette.Length; k++)
+                        {
+                            var color = colorPalette[k];
+                            // In my experience, https://en.wikipedia.org/wiki/Rectilinear_distance works better than https://en.wikipedia.org/wiki/Euclidean_distance
+                            var newDistance = color.CalculateRectilinearDistance(pixel);
+
+                            if (newDistance >= distance) continue;
+
+                            distance = newDistance;
+                            paletteIndex = k;
+                        }
+
+                        arr[j + 1][i + 1] = paletteIndex;
+                    }
+                }
+            }
+
+            return new IndexedImage(arr, colorPalette.Select(c => c.ToRgbaByteArray()).ToArray());
         }
     }
 }
