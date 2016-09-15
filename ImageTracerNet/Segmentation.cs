e08a754a51448fb00897e05117fe85ecf8cdd2c1
Michael Yanni <miyanni@microsoft.com>
2016-09-15 09:29:49 -0700

    Generalized line calculation based on the spline calculation.

--- ImageTracerNet/Segmentation.cs
+++ ImageTracerNet/Segmentation.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using ImageTracerNet.Extensions;
 using ImageTracerNet.OptionTypes;
-using PointCalculation = System.Func<double, double, double, double, double>;
+using LinePointCalculation = System.Func<double, double, double, double>;
+using SplinePointCalculation = System.Func<double, double, double, double, double>;
 
 namespace ImageTracerNet
 {
     internal static class Segmentation
     {
-        private static bool Fit(Func<int, double> distanceFunction, double threshold, int initialPathIndex, Func<int, bool> pathCondition, Func<int, int> pathStep, ref int errorPoint)
+        private static bool Fit(Func<int, Point<double>> interpPointMethod, Func<int, Point<double>> calcPointMethod, double threshold, int initialPathIndex, Func<int, bool> pathCondition, Func<int, int> pathStep, ref int errorIndex)
         {
             var pathIndices = EnumerableExtensions.ForAsRange(initialPathIndex, pathCondition, pathStep);
-            var distancesAndIndices = pathIndices.Select(i => new { Index = i, Distance = distanceFunction(i) }).ToList();
+            var distancesAndIndices = pathIndices.Select(i =>
+            {
+                var interpolatedPoint = interpPointMethod(i);
+                var calculatedPoint = calcPointMethod(i);
+                return new { Index = i, Distance = Math.Pow(interpolatedPoint.X - calculatedPoint.X, 2) + Math.Pow(interpolatedPoint.Y - calculatedPoint.Y, 2) };
+            }).ToList();
 
             // If this is true, the segment is not this line type.
             if (distancesAndIndices.Any(di => di.Distance > threshold))
             {
-                errorPoint = distancesAndIndices.Aggregate(new { Index = errorPoint, Distance = (double)0 },
+                // Finds the point index with the biggest error.
+                errorIndex = distancesAndIndices.Aggregate(new { Index = errorIndex, Distance = (double)0 },
                     (errorDi, nextDi) => nextDi.Distance > errorDi.Distance ? nextDi : errorDi).Index;
                 return false;
             }
@@ -25,31 +32,64 @@ namespace ImageTracerNet
             return true;
         }
 
-        // 5.2. Fit a straight line on the sequence
-        private static double[] FitLine(List<InterpolationPoint> path, Tracing tracingOptions, int seqStart, int seqEnd, int seqLength, out int errorPoint)
+        private static Point<double> CreateLinePoint(double pseudoIndex, Point<double> first, Point<double> second, bool isPartialPoint = false)
         {
-            var pathLength = path.Count;
-            var vx = (path[seqEnd].X - path[seqStart].X) / seqLength;
-            var vy = (path[seqEnd].Y - path[seqStart].Y) / seqLength;
-            Func<int, double> distanceFunction = i =>
-            {
-                var pl = i - seqStart;
-                pl += pl < 0 ? pathLength : 0;
-                var px = path[seqStart].X + vx*pl;
-                var py = path[seqStart].Y + vy*pl;
+            // Static Point Calculations
+            LinePointCalculation partialPointCalc = (i, start, end) => (end - start) / i;
+            LinePointCalculation endPointCalc = (i, start, partial) => start + partial * i;
 
-                return (path[i].X - px)*(path[i].X - px) + (path[i].Y - py)*(path[i].Y - py);
+            Func<double, Point<double>, Point<double>, LinePointCalculation, Point<double>> createPoint =
+                (i, p1, p2, func) => new Point<double>
+                {
+                    X = func(i, p1.X, p2.X),
+                    Y = func(i, p1.Y, p2.Y)
+                };
+
+            return createPoint(pseudoIndex, first, second, isPartialPoint ? partialPointCalc : endPointCalc);
+        }
+
+        // 5.2. Fit a straight line on the sequence
+        private static double[] FitLine(List<InterpolationPoint> path, Tracing tracingOptions, int sequenceStartIndex, int sequenceEndIndex, int sequenceLength, out int errorIndex)
+        {
+
+            //var vx = (path[sequenceEndIndex].X - path[sequenceStartIndex].X) / sequenceLength;
+            //var vy = (path[sequenceEndIndex].Y - path[sequenceStartIndex].Y) / sequenceLength;
+            //Func<> createPoint = >
+            //var midPoint = new Point<double>();
+            var startPoint = path[sequenceStartIndex];
+            var endPoint = path[sequenceEndIndex];
+
+            var partialPoint = CreateLinePoint(sequenceLength, startPoint, endPoint, true);
+            var pathLength = path.Count;
+
+            Func<int, double> pseudoIndexCalc = i =>
+            {
+                // I don't know what 'pl' as a variable name means. Is it related to path length?
+                var pl = i - sequenceStartIndex;
+                pl += pl < 0 ? pathLength : 0;
+                return pl;
             };
 
-            errorPoint = seqStart;
-            var isLine = Fit(distanceFunction, tracingOptions.LTres, (seqStart + 1) % pathLength, i => i != seqEnd, i => (i + 1) % pathLength, ref errorPoint);
+            //Func<int, double> distanceFunction = i =>
+            //{
+            //    var pl = i - sequenceStartIndex;
+            //    pl += pl < 0 ? pathLength : 0;
+            //    var px = path[sequenceStartIndex].X + vx*pl;
+            //    var py = path[sequenceStartIndex].Y + vy*pl;
+
+            //    return (path[i].X - px)*(path[i].X - px) + (path[i].Y - py)*(path[i].Y - py);
+            //};
+
+            errorIndex = sequenceStartIndex;
+            var isLine = Fit(i => path[i], i => CreateLinePoint(pseudoIndexCalc(i), startPoint, partialPoint), tracingOptions.LTres, 
+                (sequenceStartIndex + 1) % pathLength, i => i != sequenceEndIndex, i => (i + 1) % pathLength, ref errorIndex);
             return isLine ? new[]
             {
                 1.0,
-                path[seqStart].X,
-                path[seqStart].Y,
-                path[seqEnd].X,
-                path[seqEnd].Y,
+                path[sequenceStartIndex].X,
+                path[sequenceStartIndex].Y,
+                path[sequenceEndIndex].X,
+                path[sequenceEndIndex].Y,
                 0.0,
                 0.0
             } : null;
@@ -63,13 +103,13 @@ namespace ImageTracerNet
             Func<double, double> t3Calc = t => Math.Pow(t, 2);
 
             // Static Point Calculations
-            PointCalculation midPointCalc =
+            SplinePointCalculation midPointCalc =
                 (i, start, end, fit) => (t1Calc(i) * start + t3Calc(i) * end - fit) / -t2Calc(i);
 
-            PointCalculation finalPointCalc =
+            SplinePointCalculation finalPointCalc =
                 (i, start, mid, end) => t1Calc(i) * start + t2Calc(i) * mid + t3Calc(i) * end;
 
-            Func<double, Point<double>, Point<double>, Point<double>, PointCalculation, Point<double>> createPoint =
+            Func<double, Point<double>, Point<double>, Point<double>, SplinePointCalculation, Point<double>> createPoint =
                 (i, p1, p2, p3, func) => new Point<double>
                 {
                     X = func(i, p1.X, p2.X, p3.X),
@@ -83,20 +123,20 @@ namespace ImageTracerNet
         // helpers and projecting to get control point
         private static double[] FitSpline(List<InterpolationPoint> path, Tracing tracingOptions, int sequenceStartIndex, int sequenceEndIndex, int sequenceLength, ref int errorIndex)
         {
-            // Create the spline closure using path data.
             var startPoint = path[sequenceStartIndex];
             var endPoint = path[sequenceEndIndex];
             var fitPoint = path[errorIndex];
 
             Func<int, double> pseudoIndexCalc = i => (i - sequenceStartIndex) / (double)sequenceLength;
             var midPoint = CreateSplinePoint(pseudoIndexCalc(errorIndex), startPoint, endPoint, fitPoint, true);
-            Func<int, double> distanceFunction = i =>
-            {
-                var point = CreateSplinePoint(pseudoIndexCalc(i), startPoint, midPoint, endPoint);
-                return Math.Pow(path[i].X - point.X, 2) + Math.Pow(path[i].Y - point.Y, 2);
-            };
+            //Func<int, double> distanceFunction = i =>
+            //{
+            //    var point = CreateSplinePoint(pseudoIndexCalc(i), startPoint, midPoint, endPoint);
+            //    return Math.Pow(path[i].X - point.X, 2) + Math.Pow(path[i].Y - point.Y, 2);
+            //};
             // Check every point
-            var isSpline = Fit(distanceFunction, tracingOptions.QTres, sequenceStartIndex + 1, i => i != sequenceEndIndex, i => (i + 1) % path.Count, ref errorIndex);
+            var isSpline = Fit(i => path[i], i => CreateSplinePoint(pseudoIndexCalc(i), startPoint, midPoint, endPoint), tracingOptions.QTres, 
+                sequenceStartIndex + 1, i => i != sequenceEndIndex, i => (i + 1) % path.Count, ref errorIndex);
             return isSpline ? new[]
             {
                 2.0,
