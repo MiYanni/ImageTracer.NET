e05236ac475fb6a0fdb51e49b41fbffc8d9fd11b
Michael Yanni <miyanni@microsoft.com>
2016-09-15 15:19:51 -0700

    Moved the sequence methods into Sequencing.cs.

--- ImageTracerNet/ImageTracer.cs
+++ ImageTracerNet/ImageTracer.cs
@@ -174,49 +174,9 @@ namespace ImageTracerNet
         //
         // path type is discarded, no check for path.size < 3 , which should not happen
 
-        private static int DetermineSequenceEndIndex(IReadOnlyList<Heading> directions, int pathIndex)
-        {
-            var pathDirection = directions[pathIndex];
-            var lastIndex = directions.Count - 1;
-
-            var sequenceEndIndex = pathIndex + 1;
-            Heading? storedDirection = null;
-            for (var i = sequenceEndIndex; i < lastIndex; sequenceEndIndex = ++i)
-            {
-                if (!(storedDirection == null || directions[i] == storedDirection || directions[i] == pathDirection))
-                {
-                    break;
-                }
-
-                if (directions[i] != pathDirection && storedDirection == null)
-                {
-                    storedDirection = directions[i];
-                }
-            }
-
-            // If it gets to the end of the list, return zero instead of the index.
-            return sequenceEndIndex == lastIndex ? 0 : sequenceEndIndex;
-        }
-
-        private static IEnumerable<Tuple<int, int>> CreateSequences(IReadOnlyList<Heading> directions)
-        {
-            int sequenceEndIndex;
-            for (var i = 0; i < directions.Count; i = sequenceEndIndex)
-            {
-                sequenceEndIndex = DetermineSequenceEndIndex(directions, i);
-                yield return new Tuple<int, int>(i, sequenceEndIndex);
-
-                // If the end index is the last index of the list (will be 0 from DetermineSequenceEndIndex), then we have all of the sequences.
-                if (!(sequenceEndIndex > 0))
-                {
-                    yield break;
-                }
-            }
-        }
-
         private static IEnumerable<double[]> TracePath(List<InterpolationPoint> path, Tracing tracingOptions)
         {
-            var sequences = CreateSequences(path.Select(p => p.Direction).ToList());
+            var sequences = Sequencing.Create(path.Select(p => p.Direction).ToList());
             // Fit the sequences into segments, and return them.
             return sequences.Select(s => Segmentation.Fit(path, tracingOptions, s.Item1, s.Item2)).SelectMany(s => s);
         }
---
+++ ImageTracerNet/Sequencing.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTracerNet
+{
+    internal static class Sequencing
+    {
+        private static int DetermineSequenceEndIndex(IReadOnlyList<Heading> directions, int pathIndex)
+        {
+            var pathDirection = directions[pathIndex];
+            var lastIndex = directions.Count - 1;
+
+            var sequenceEndIndex = pathIndex + 1;
+            Heading? storedDirection = null;
+            for (var i = sequenceEndIndex; i < lastIndex; sequenceEndIndex = ++i)
+            {
+                if (!(storedDirection == null || directions[i] == storedDirection || directions[i] == pathDirection))
+                {
+                    break;
+                }
+
+                if (directions[i] != pathDirection && storedDirection == null)
+                {
+                    storedDirection = directions[i];
+                }
+            }
+
+            // If it gets to the end of the list, return zero instead of the index.
+            return sequenceEndIndex == lastIndex ? 0 : sequenceEndIndex;
+        }
+
+        public static IEnumerable<Tuple<int, int>> Create(IReadOnlyList<Heading> directions)
+        {
+            int sequenceEndIndex;
+            for (var i = 0; i < directions.Count; i = sequenceEndIndex)
+            {
+                sequenceEndIndex = DetermineSequenceEndIndex(directions, i);
+                yield return new Tuple<int, int>(i, sequenceEndIndex);
+
+                // If the end index is the last index of the list (will be 0 from DetermineSequenceEndIndex), then we have all of the sequences.
+                if (!(sequenceEndIndex > 0))
+                {
+                    yield break;
+                }
+            }
+        }
+    }
+}
