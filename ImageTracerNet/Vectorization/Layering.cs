da76bda33b3ea9ed6f11064a230184af143e566c
Michael Yanni <miyanni@microsoft.com>
2016-09-22 11:27:45 -0700

    Minor cleanup in Layering prior to flattening.

--- ImageTracerNet/Vectorization/Layering.cs
+++ ImageTracerNet/Vectorization/Layering.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ImageTracerNet.Extensions;
+using EdgeNodeCalc = System.Func<ImageTracerNet.ColorGroup, ImageTracerNet.Vectorization.EdgeNode>;
 using static System.Convert;
 
 namespace ImageTracerNet.Vectorization
 {
     internal static class Layering
     {
+        // 1, 3, 5, 7, 9, 11, 13, 15
+        private static readonly EdgeNodeCalc BottomRight = 
+            cg => (EdgeNode)(1 + ToInt32(cg.MidRight == cg.Mid) * 2 + ToInt32(cg.BottomRight == cg.Mid) * 4 + ToInt32(cg.BottomMid == cg.Mid) * 8);
+        // 2, 6, 10, 14
+        private static readonly EdgeNodeCalc BottomMid =
+            cg => (EdgeNode)(2 + ToInt32(cg.BottomMid == cg.Mid) * 4 + ToInt32(cg.BottomLeft == cg.Mid) * 8);
+        // 8, 10, 12, 14
+        private static readonly EdgeNodeCalc MidRight =
+            cg => (EdgeNode)(8 + ToInt32(cg.TopRight == cg.Mid) * 2 + ToInt32(cg.MidRight == cg.Mid) * 4);
+        // 4, 6, 12, 14
+        private static readonly EdgeNodeCalc Mid =
+            cg => (EdgeNode)(4 + ToInt32(cg.TopMid == cg.Mid) * 2 + ToInt32(cg.MidLeft == cg.Mid) * 8);
+
         // 2. Layer separation and edge detection
 
         // Edge node types ( ▓:light or 1; ░:dark or 0 )
@@ -25,30 +39,18 @@ namespace ImageTracerNet.Vectorization
             {
                 // Are neighbor pixel colors the same?
                 // this pixel's type and looking back on previous pixels
-                // X
-                // 1, 3, 5, 7, 9, 11, 13, 15
-                layers[cg.Mid][cg.X + 1][cg.Y + 1] =
-                    (EdgeNode)(1 + ToInt32(cg.MidRight == cg.Mid) * 2 + ToInt32(cg.BottomRight == cg.Mid) * 4 + ToInt32(cg.BottomMid == cg.Mid) * 8);
+                layers[cg.Mid][cg.X + 1][cg.Y + 1] = BottomRight(cg);
                 if (cg.MidLeft != cg.Mid)
                 {
-                    // A
-                    // 2, 6, 10, 14
-                    layers[cg.Mid][cg.X + 1][cg.Y] =
-                        (EdgeNode)(2 + ToInt32(cg.BottomMid == cg.Mid) * 4 + ToInt32(cg.BottomLeft == cg.Mid) * 8);
+                    layers[cg.Mid][cg.X + 1][cg.Y] = BottomMid(cg);
                 }
                 if (cg.TopMid != cg.Mid)
                 {
-                    // B
-                    // 8, 10, 12, 14
-                    layers[cg.Mid][cg.X][cg.Y + 1] =
-                        (EdgeNode)(8 + ToInt32(cg.TopRight == cg.Mid) * 2 + ToInt32(cg.MidRight == cg.Mid) * 4);
+                    layers[cg.Mid][cg.X][cg.Y + 1] = MidRight(cg);
                 }
                 if (cg.TopLeft != cg.Mid)
                 {
-                    // C
-                    // 4, 6, 12, 14
-                    layers[cg.Mid][cg.X][cg.Y] =
-                        (EdgeNode)(4 + ToInt32(cg.TopMid == cg.Mid) * 2 + ToInt32(cg.MidLeft == cg.Mid) * 8);
+                    layers[cg.Mid][cg.X][cg.Y] = Mid(cg);
                 }
             }
 
