8b493217eeb41f34b4cf023105ca04800fdc17dc
Michael Yanni <miyanni@microsoft.com>
2016-09-23 15:16:37 -0700

    Completed cleanup of Interpolation. Found that parallelization was less efficient. Investigate later.

--- ImageTracerNet/Vectorization/Interpolation.cs
+++ ImageTracerNet/Vectorization/Interpolation.cs
@@ -14,34 +14,17 @@ namespace ImageTracerNet.Vectorization
             // interpolate between two path points
             var pp2 = path[(index + 1) % path.Count];
             var pp3 = path[(index + 2) % path.Count];
-            var other = new InterpolationPoint(pp2, pp3);
+            var next = new InterpolationPoint(pp2, pp3);
 
             // line segment direction to the next point
-            return new InterpolationPoint(pp1, pp2, other);
+            return new InterpolationPoint(pp1, pp2, next);
         }
 
         // 4. interpolating between path points for nodes with 8 directions ( East, SouthEast, S, SW, W, NW, N, NE )
         public static IEnumerable<IEnumerable<InterpolationPoint>> Convert(IEnumerable<IReadOnlyList<PathPoint>> paths)
         {
-            //var ins = new List<List<InterpolationPoint>>();
-
-            // paths loop
-            foreach (var path in paths)
-            {
-                yield return path.Select((p, i) => CreatePoint(path, i));
-
-                //var thisInp = new List<InterpolationPoint>();
-                //ins.Add(thisInp);
-                //var pathLength = path.Count;
-
-                // pathpoints loop
-                //for (var pointIndex = 0; pointIndex < pathLength; pointIndex++)
-                //{
-                //    thisInp.Add(CreatePoint(path, pointIndex));
-                //}
-            }
-
-            //return ins;
+            // TODO: It was less efficient to parallelize these calls. Investigate later.
+            return paths.Select(path => path.Select((p, i) => CreatePoint(path, i)));
         }
     }
 }
