b5632fe8b0653400e7384a6d5398e6178590237c
Michael Yanni <miyanni@microsoft.com>
2016-09-26 09:24:53 -0700

    Finally figured out how to get the Aggregate to work for the sequence end index. I needed to both go until lastIndex inclusive and have a DefaultIfEmpty value.

--- ImageTracerNet/Vectorization/Sequencing.cs
+++ ImageTracerNet/Vectorization/Sequencing.cs
@@ -1,35 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ImageTracerNet.Vectorization
 {
     internal static class Sequencing
     {
+        //private static int Original(IReadOnlyList<Heading> directions, int initialIndex)
+        //{
+        //    var initialDirection = directions[initialIndex];
+        //    var lastIndex = directions.Count - 1;
+
+        //    int sequenceEndIndex;
+        //    Heading? storedDirection = null;
+        //    for (sequenceEndIndex = initialIndex + 1; sequenceEndIndex < lastIndex; ++sequenceEndIndex)
+        //    {
+        //        var direction = directions[sequenceEndIndex];
+        //        if (!(storedDirection == null || direction == storedDirection || direction == initialDirection))
+        //        {
+        //            return sequenceEndIndex;
+        //        }
+
+        //        // This initializes storedDirection at some point during the loop.
+        //        if (storedDirection == null && direction != initialDirection)
+        //        {
+        //            storedDirection = direction;
+        //        }
+        //    }
+
+        //    return 0;
+        //}
+
         private static int DetermineSequenceEndIndex(IReadOnlyList<Heading> directions, int initialIndex)
         {
             var initialDirection = directions[initialIndex];
             var lastIndex = directions.Count - 1;
-
-            int sequenceEndIndex;
             Heading? storedDirection = null;
-            for (sequenceEndIndex = initialIndex + 1; sequenceEndIndex < lastIndex; ++sequenceEndIndex)
-            {
-                var direction = directions[sequenceEndIndex];
-                if (!(storedDirection == null || direction == storedDirection || direction == initialDirection))
+            //http://stackoverflow.com/questions/8867867/sequence-contains-no-elements-exception-in-linq-without-even-using-single
+            var sequenceEndIndex = directions.Select((d, i) => new { Direction = d, Index = i }).Where(di => di.Index >= initialIndex + 1 && di.Index <= lastIndex)
+                .DefaultIfEmpty(new { Direction = Heading.Center, Index = lastIndex })
+                .Aggregate((di, diNext) =>
                 {
-                    return sequenceEndIndex;
-                }
+                    if (!(storedDirection == null || di.Direction == storedDirection || di.Direction == initialDirection))
+                    {
+                        return di;
+                    }
 
-                // This initializes storedDirection at some point during the loop.
-                if (storedDirection == null && direction != initialDirection)
-                {
-                    storedDirection = direction;
-                }
-            }
+                    // This initializes storedDirection at some point during the loop.
+                    if (storedDirection == null && di.Direction != initialDirection)
+                    {
+                        storedDirection = di.Direction;
+                    }
+
+                    return diNext;
+                }).Index;
 
             // If it gets to the end of the list, return zero instead of the index.
-            //return sequenceEndIndex == lastIndex ? 0 : sequenceEndIndex;
-            return 0;
+            return sequenceEndIndex == lastIndex ? 0 : sequenceEndIndex;
         }
 
         public static IEnumerable<SequenceIndices> Create(IReadOnlyList<Heading> directions)
