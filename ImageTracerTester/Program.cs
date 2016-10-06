83068ac2e8c7352c803feeed54b3bd0ccf1f71b1
Michael Yanni <miyanni@microsoft.com>
2016-10-06 16:20:18 -0700

    Adjusting some of the parameters for making the non-GUI image.

--- ImageTracerTester/Program.cs
+++ ImageTracerTester/Program.cs
@@ -16,7 +16,16 @@ namespace ImageTracerTester
         private static void Main(string[] args)
         {
             //SaveTracedImage(args);
-            SaveTracedImage(new [] { @"..\..\Images\Chrono Trigger2.png", "outfilename", @"chronotrigger2-traced-new.svg", "ltres", "0.1", "qtres", "1", "scale", "44", "numberofcolors", "256", "pathomit", "0" });
+            SaveTracedImage(new []
+            {
+                @"..\..\Images\Chrono Trigger2.png",
+                "outfilename", @"chronotrigger2-traced-new.svg",
+                "ltres", ".22",
+                "qtres", "20",
+                "scale", "5",
+                "numberofcolors", "256",
+                "pathomit", "0"
+            });
             //ColorArrayTest(100, 200);
             //GaussianBlurTest(@"..\..\Images\Chrono Trigger2.png", @"chronotrigger2-blurred.png");
             //ColorPaletteTest(@"..\..\Images\Chrono Trigger2.png", @"chronotrigger2-palette.png", 8, 8);
